e070f406d80e2badeb4feb05ef5a0648f106bd4b
Kuobigman <Kuobigman@yahoo.com.tw>
2020-04-17 15:11:53 +0800

    updated PYA909.py

--- PYA909.py
+++ PYA909.py
@@ -2,11 +2,16 @@ s1 = list()
 for i in range(5):
 	s1 += [input()]
 
-with open("data.dat", "w") as fn:
-    for item in s1:
-        fn.write(str(item)+"\n")
 
+with open("data.dat", "w") as fn:
+    for i in range(len(s1)):
+        if i < len(s1)-1:
+            fn.write(str(s1[i])+"\n\n")
+        else:
+            fn.write(str(s1[i]))
+            
 with open("data.dat", "r") as fn:
   s2 = fn.read()
 
+print('The content of "data.dat":')
 print(s2)
